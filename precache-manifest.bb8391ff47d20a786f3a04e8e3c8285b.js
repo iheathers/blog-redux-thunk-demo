71ea9823fc721647bc375fc8ff2ee2f84450a663
iheathers <shresthapritam7@gmail.com>
2020-07-29 19:31:21 +0545

    Updates

---
+++ precache-manifest.bb8391ff47d20a786f3a04e8e3c8285b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b050c3725aa543c8343fe38d8246bd2",
+    "url": "/blog-redux-thunk-demo/index.html"
+  },
+  {
+    "revision": "5459655a5110c704b857",
+    "url": "/blog-redux-thunk-demo/static/js/2.3616c39e.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/blog-redux-thunk-demo/static/js/2.3616c39e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c0403b0bdfe466be714",
+    "url": "/blog-redux-thunk-demo/static/js/main.e51f1b6b.chunk.js"
+  },
+  {
+    "revision": "aa5e51019e8e6899f9b4",
+    "url": "/blog-redux-thunk-demo/static/js/runtime-main.6fdaef14.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/blog-redux-thunk-demo/precache-manifest.bb8391ff47d20a786f3a04e8e3c8285b.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/blog-redux-thunk-demo/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
